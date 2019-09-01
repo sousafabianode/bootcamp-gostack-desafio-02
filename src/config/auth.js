7aa9beccbc857696d8804204379a457d7d8eb265
Fabiano <sousa.fabianode@gmail.com>
2019-09-01 00:35:09 -0300

    New Files

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'fa945254755a01b0632c3f8ada490b4f',
+  expiresIn: '7d',
+};
---
+++ src/config/systemMessages.js
@@ -0,0 +1,8 @@
+export default {
+  INVALID_CREDENTIALS: 'Invalid credentials!',
+  USER_NOT_FOUND: 'User not found!',
+  VALIDATION_FAILS: 'Validation fails!',
+  USER_ALREADY_EXISTS: 'User already exists!',
+  UNPROVIDED_TOKEN: 'Unprovided Token!',
+  INVALID_TOKEN: 'Invalid Token!',
+};
